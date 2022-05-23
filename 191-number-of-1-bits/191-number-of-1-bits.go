1a4c2a2190dbb9e428b5c64021947ebcde5344a5
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-05-23 13:23:23 +0300

    Time: 0 ms (100.00%), Space: 2 MB (41.60%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.go
+++ 191-number-of-1-bits/191-number-of-1-bits.go
@@ -1,13 +1,10 @@
 func hammingWeight(num uint32) int {
     
     numOfBits := 0
-    for i := 0; i < 32; i++ {
+    for num > 0 {
         
-        if num & 1 == 1 {
-            
-            numOfBits++
-        }
-        num = num >> 1
+        num = num & (num - 1)
+        numOfBits++
     }
     return numOfBits
 }

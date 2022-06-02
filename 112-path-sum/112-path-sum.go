95e0b2cd5db8b68622ee02ec0094992fb7ff3695
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-06-02 10:42:56 +0300

    Time: 3 ms (90.33%), Space: 4.6 MB (88.60%) - LeetHub

---
+++ 112-path-sum/112-path-sum.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    
+    if root == nil {
+        
+        return false
+    }
+    
+    if root.Left == nil && root.Right == nil && targetSum - root.Val == 0 {
+        
+        return true
+    }
+    
+    return hasPathSum( root.Left, targetSum - root.Val) || hasPathSum( root.Right, targetSum - root.Val)
+}

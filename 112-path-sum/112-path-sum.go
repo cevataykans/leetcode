cdb6504fa70a5c77385e7398f315c05aea0db0f2
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-06-02 10:44:23 +0300

    Time: 3 ms (90.33%), Space: 4.7 MB (70.64%) - LeetHub

--- 112-path-sum/112-path-sum.go
+++ 112-path-sum/112-path-sum.go
@@ -13,9 +13,9 @@ func hasPathSum(root *TreeNode, targetSum int) bool {
         return false
     }
     
-    if root.Left == nil && root.Right == nil && targetSum - root.Val == 0 {
-        
-        return true
+    if root.Left == nil && root.Right == nil {
+    
+        return targetSum - root.Val == 0
     }
     
     return hasPathSum( root.Left, targetSum - root.Val) || hasPathSum( root.Right, targetSum - root.Val)

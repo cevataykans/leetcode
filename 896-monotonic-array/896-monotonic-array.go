3b0fab30650895d6562e9e7cbd33d048540f8d6a
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-06-03 17:08:56 +0300

    Time: 333 ms (5.33%), Space: 12 MB (5.33%) - LeetHub

---
+++ 896-monotonic-array/896-monotonic-array.go
@@ -0,0 +1,27 @@
+func isMonotonic(nums []int) bool {
+    
+    if len( nums) <= 2 {
+        
+        return true
+    }
+    
+    isGreat, isLow := false, false
+    for i := 1; i < len( nums); i++ {
+        
+        if nums[ i] > nums[ i - 1] {
+            
+            isGreat = true
+        }
+        
+        if nums[ i] < nums[ i - 1] {
+            
+            isLow = true
+        }
+        
+        if isGreat && isLow {
+            
+            return false
+        }
+    }
+    return true
+}

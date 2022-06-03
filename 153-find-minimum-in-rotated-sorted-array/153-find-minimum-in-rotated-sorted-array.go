299b0d5495e362fa7517645b776cba4d59e0e89f
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-06-03 15:11:38 +0300

    Time: 5 ms (30.04%), Space: 2.5 MB (69.96%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,16 @@
+func findMin(nums []int) int {
+    
+    left, right := 0, len( nums) - 1
+    for left < right {
+        
+        middle := left + (right - left) / 2
+        if nums[ middle] > nums[ right] {
+            
+            left = middle + 1
+        } else {
+            
+            right = middle
+        }
+    }
+    return nums[ left]
+}

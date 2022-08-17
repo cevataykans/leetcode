8eb941889a8da620e3a198248d879a3f80ccc951
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-08-17 16:10:03 +0300

    Time: 159 ms (70.40%), Space: 9 MB (46.62%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -1,19 +1,13 @@
 func maxProfit(prices []int) int {
     
-    maxSoFar := 0
-    max := 0
-    for i := 1; i < len( prices); i++ {
-        
-        maxSoFar = maxInt( 0, maxSoFar + prices[ i] - prices[ i - 1])
-        max = maxInt( max, maxSoFar)
+    profit := 0
+    buyPrice := prices[ 0]
+    for i := 1; i < len(prices); i++ {
+        if prices[ i] < buyPrice {
+            buyPrice = prices[ i]
+        } else if profit < prices[ i] - buyPrice {
+            profit = prices[ i] - buyPrice
+        }
     }
-    return max
-}
-
-func maxInt( a int, b int) int {
-    
-    if a < b {
-        return b
-    }
-    return a
+    return profit
 }

4c1304da29a4867c0aa8716374dc11df7a5828ea
Cevat Aykan Sevinç <aykansevinc66@gmail.com>
2022-08-17 17:17:13 +0300

    Time: 0 ms (100.00%), Space: 2.5 MB (70.83%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
@@ -1,41 +1,35 @@
 func search(nums []int, target int) int {
     
-    left, right := 0, len( nums) - 1
-    for left <= right {
-        
-        middle := left + (right - left) / 2
-        if nums[ middle] == target {
-            
-            return middle
-        }
-        
-        var comparator int
-        if (target >= nums[0]) == (nums[middle] >= nums[0]) {
-            
-            comparator = nums[ middle]
-            
-        } else {
-            
-            if target < nums[ 0] {
-                
-                comparator = math.MinInt
-            } else {
-                
-                comparator = math.MaxInt
-            }
-        }
-        
-        if target == comparator {
-            
-            return middle
-        } else if target > comparator {
-            
-            left = middle + 1
-        } else {
-            
-            right = middle - 1
-        }
-        
+    return searchReverseBinary( nums, 0, len(nums) - 1, target)
+}
+
+func searchReverseBinary( nums []int, l, r, target int) int {
+    
+    if l > r {
+        return -1
     }
-    return -1
+    
+    mid := (r - l) / 2 + l
+    var comp int
+    if (target >= nums[ 0]) == (nums[mid] >= nums[ 0]) {
+        
+        comp = nums[ mid]
+        
+    } else {
+        
+        if nums[ 0] > target {
+            comp = math.MinInt
+        } else {
+            comp = math.MaxInt
+        }
+    }
+    
+    if comp == target {
+        return mid
+    } 
+    
+    if comp < target {
+        return searchReverseBinary( nums, mid + 1, r, target)
+    }
+    return searchReverseBinary( nums, l, mid - 1, target)
 }
